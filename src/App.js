c709cf4c9061e15b62d1e1f2a2a13f9fac7cba25
axror <abduxolikov.axror@gmail.com>
2023-05-03 10:45:13 +0500

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Offers from "./pages/Offers"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import ForgetPassword from "./pages/ForgetPassword"
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/offers" element={<Offers/>} />
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/sign-in" element={<SignIn/>} />
+          <Route path="/sign-up" element={<SignUp/>} />
+          <Route path="/forget-password" element={<ForgetPassword/>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
