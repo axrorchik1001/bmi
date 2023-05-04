d0b8c0cbdb87c34a989d52186b5f65a6dad9ef50
axror <abduxolikov.axror@gmail.com>
2023-05-04 18:31:29 +0500

    repair navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,87 @@
-import { useLocation } from "react-router-dom";
-// import React from "react";
+import { useLocation, Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function Header() {
+  const [activeLink, setActiveLink] = useState("");
+  // const [defaultActiveLink, setDefaultActiveLink] = useState("true")
+
+  const handleClick = (path) => {
+    setActiveLink(path);
+    // setDefaultActiveLink("false")
+  };
+
+  // const handleDefault = () => {
+  //   setDefaultActiveLink("");
+  // }
+
+  useEffect(() => {
+    setActiveLink("/")
+  },[])
+
   const location = useLocation();
+  const navigate = useNavigate();
   console.log(location.pathname);
-  function pathMathRouter(route) {
-    if (route === location.pathname) {
-      return true
-    }
-  }
+
   return (
     <div className="border-b shadow-sm sticky top-0 z-50 ">
       <header className="flex justify-between items-center max-w-6xl mx-auto ">
         <div>
-          <p className="text-2xl text-color cursor-pointer ">Turar Joy </p>
+          <p className="text-2xl text-color cursor-pointer " onClick={()=> {
+            navigate("/")
+            setActiveLink("/")
+          }}>Turar Joy </p>
         </div>
         <div>
-            <ul className="flex  space-x-10">
-            <li
-              className={`py-3 font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMathRouter("/") && "text-[#000] border-b-red-400"
-              }`}
-            >
-              Home
+          <ul className="flex  space-x-10">
+            <li>
+              <Link
+                to="/"
+                className={`py-3 px-4 block ${
+                  activeLink === "/"
+                    ? "text-black border-b-[3px] border-b-red-500 "
+                    : "text-gray-400 "
+                }
+                `}
+                
+                onClick={() => {
+                  handleClick("/")
+                  
+                } }
+              >
+                Home
+              </Link>
             </li>
-
-            <li
-              className={`py-3 font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMathRouter("/offers") && "text-[#000] border-b-red-400 "
-              }`}
-            >
-              Offers
+            <li>
+              <Link
+                to="/offers"
+                className={`py-3 px-4 block  ${
+                  activeLink === "/offers"
+                    ? "text-black border-b-[3px] border-b-red-500 "
+                    : "text-gray-400 "
+                }`}
+                onClick={() => {
+                  handleClick("/offers")
+                  // handleDefault("false")
+                }}
+              >
+                Offers
+              </Link>
             </li>
-
-            <li
-              className={`py-3 font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMathRouter("/sign-in") && "text-[#000] border-b-red-400 " 
-              }`}
-            >
-              Sign In
+            <li>
+              <Link
+                to="/sign-in"
+                className={`py-3 px-4 block  ${
+                  activeLink === "/sign-in"
+                    ? "text-black border-b-[3px] border-b-red-500 "
+                    : "text-gray-400 "
+                }`}
+                onClick={() => {
+                  handleClick("/sign-in")
+                  // handleDefault()
+                }}
+              >
+                Sign In
+              </Link>
             </li>
           </ul>
         </div>
