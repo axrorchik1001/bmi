5f081b506962504cc545916da1dde0ad4052b32b
axror <abduxolikov.axror@gmail.com>
2023-05-09 13:10:16 +0500

    create private custom hook and route protection the profile page and add  the logout functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import ForgetPassword from "./pages/ForgetPassword";
+import PrivateRoute from "./components/PrivateRoute";
+import ForgotPassword from "./pages/ForgotPassword";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <>
@@ -16,11 +16,14 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/offers" element={<Offers />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/forget-password" element={<ForgetPassword />} />
+          <Route path="/forget-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
         </Routes>
       </Router>
       <ToastContainer
@@ -35,9 +38,7 @@ function App() {
         pauseOnHover
         theme="dark"
       />
-      {/* yuqoridagi notifications kodi react-toastify */}
     </>
   );
 }
-
 export default App;
